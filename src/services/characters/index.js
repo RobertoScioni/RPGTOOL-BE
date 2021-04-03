6d12e4cf93cba088729f8a97e0e99f1e8c75c087
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-04-03 02:42:17 +0200

    fix to not overwrite template sheet

--- src/services/characters/index.js
+++ src/services/characters/index.js
@@ -28,90 +28,6 @@ charactersRouter.get("/", authorize, async (req, res, next) => {
 	}
 })
 
-const templateSheet = {
-	Counters: [
-		{
-			name: "Physical Stress",
-			min: 0,
-			value: 0,
-			max: 2,
-			abbreviation: "PS",
-		},
-		{
-			name: "Mental Stress",
-			min: 0,
-			value: 0,
-			max: 2,
-			abbreviation: "MS",
-		},
-		{ name: "Fate Points", min: 0, value: 2, max: 2, abbreviation: "FP" },
-	],
-	Pages: {
-		Skills: [
-			{ name: "Lore", macro: "[4dF+4]" },
-			{ name: "Rapport", macro: "[4dF+3]" },
-			{ name: "Crafts", macro: "[4dF+3]" },
-			{ name: "Athletics", macro: "[4dF+2]" },
-			{ name: "Will", macro: "[4dF+2]" },
-			{ name: "Investigate", macro: "[4dF+2]" },
-			{ name: "Fight", macro: "[4dF+1]" },
-			{ name: "Resources", macro: "[4dF+1]" },
-			{ name: "Contacts", macro: "[4dF+1]" },
-			{ name: "Notice", macro: "[4dF+1]" },
-		],
-		Aspects: [
-			{ name: "Wizard for hire" },
-			{ name: "Rivals in the collegia arcana" },
-			{ name: "If i haven't been there, i've read about it" },
-			{ name: "not the face!" },
-			{ name: "doesn't suffer fools gladly" },
-		],
-
-		Consequences: [
-			{ name: "Mild", value: "" },
-			{ name: "Moderate", value: "" },
-			{ name: "Severe", value: "" },
-		],
-		extras: [],
-		Stunts: [
-			{
-				name: "Scholar,healer",
-				dsc: "can attempt physical recovery using Lore",
-			},
-			{
-				name: "Friendly Liar",
-				dsc:
-					"Can use Rapport in place of Deceive to create advantages predicated on a lie.",
-			},
-			{
-				name: "The Power of Deduction",
-				dsc: ` 
-					Once per scene you can spend a fate point
-					(and a few minutes of observation) to make 
-					a special Investigate roll 
-					representing your potent deductive faculties.
-					For each shift you make on this roll you discover 
-					or create an aspect, on either the scene or	
-					the target of your observations, 
-					though you may only invoke one of them for free.
-				`,
-			},
-			{
-				name: "I’ve Read about That!",
-				dsc: `
-					You’ve read hundreds—if not thousands—of
-					books on a wide variety of topics. 
-					You can spend a fate point to use
-					Lore in place of any other skill 
-					for one roll or exchange, provided you
-					can justify having read about 
-					the action you’re attempting.
-				`,
-			},
-		],
-	},
-}
-
 charactersRouter.get("/:id", authorize, async (req, res, next) => {
 	try {
 		const profile = await CharacterModel.findById(req.params.id)
@@ -134,7 +50,7 @@ charactersRouter.post("/", authorize, async (req, res, next) => {
 	try {
 		const newCharacter = new CharacterModel(req.body)
 		newCharacter.owner = req.user._id
-		newCharacter.sheet = templateSheet
+
 		console.log("null? ->", newCharacter._id)
 		const { _id } = await newCharacter.save()
 		console.log("save this inside the user pls", _id)
