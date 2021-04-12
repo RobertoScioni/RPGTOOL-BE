9692b9d0b8e99fa62f7079f721f9d26a86187bd6
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-04-12 19:44:35 +0200

    -

--- src/services/campaigns/index.js
+++ src/services/campaigns/index.js
@@ -20,7 +20,7 @@ campaignsRouter.get("/", authorize, async (req, res, next) => {
 		console.log("**********GET Campaign LIST**********")
 		console.log(req.user)
 		const users = await CampaignsModel.find({
-			"members._id": ObjectId("604b0bace15419189ac24ddd"),
+			"members._id": `ObjectId(${user._id})`,
 		})
 		console.log(users)
 		res.send(users)
